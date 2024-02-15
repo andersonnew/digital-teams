988a290bebe84a5238c4e1b01927005bdc1f50ba
andersonnew <andersontici@gmail.com>
2024-02-15 12:02:01 -0300

    continuidade

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let teams = []
 criarBtn.onclick = () =>{
     //alert("clicou");
     //console.log("clicou aqui");
@@ -16,18 +17,22 @@ overlay.onclick = () => {
 
 formCriar.onsubmit = () => {
     event.preventDefault();
-
-    alert(nome.value+" "+capacidade.value);
-listTeams.innerHTML = ' ';
-listTeams.innerHTML = ` <li>
-<h4>nome do team <box-icon name='show'></box-icon></h4>
-<h1>0<span>/0</span></h1>
-<div class="actions">
-    <button>adicionar</button>
-    <button><box-icon name='trash' ></box-icon></button>
-</div>
-</li>`;
-    overlay.classList.remove("show");
-    formCriar.classList.remove("show");
+    teams.push({
+        name: Name.value,
+        capacity: capacidade.value,
+        menbers:[]
+    });
+   // alert(nome.value+" "+capacidade.value);
+//listTeams.innerHTML = ' ';
+//listTeams.innerHTML = ` <li>
+//<h4>${nome.value}<box-icon name='show'></box-icon></h4>
+//<h1>0<span>/${capacidade.value}</span></h1>
+//<div class="actions">
+  //  <button>adicionar</button>
+  //  <button><box-icon name='trash' ></box-icon></button>
+//</div>
+//</li>`;
+  //  overlay.classList.remove("show");
+  //  formCriar.classList.remove("show");
 }
 
